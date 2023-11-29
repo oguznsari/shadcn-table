6be267913f2311cebd42eb966bae04d402b9195a
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-11-29 17:56:03 +0300

    Table component

---
+++ app/users/columns.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  lastSeen: string;
+};
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "lastSeen",
+    header: "Last Seen",
+  },
+];
